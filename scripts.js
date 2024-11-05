370524e9398cd8059eb82a3371303da65bbe4046
Caio Balduino <caiobalduinodev@hotmail.com>
2024-11-05 20:57:59 -0300

    💫 feat: create total calculate & format

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
 const expenseList = document.querySelector("ul");
+const expensesTotal = document.querySelector("aside header h2");
 const expensesQuantity = document.querySelector("aside header p span");
 
 amount.oninput = () => {
@@ -98,6 +99,44 @@ function updateTotals() {
     expensesQuantity.textContent = `${items.length} ${
       items.length > 1 ? "despesas" : "despesa"
     }`;
+
+    //Variável para incrementar o total.
+    let total = 0;
+    //Percorre cada item (li) da lista.
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector(".expense-amount");
+
+      //Remover caracteres não numéricos e substitui a vírgula pelo ponto.
+      let value = itemAmount.textContent
+        .replace(/[^\d,]/g, "")
+        .replace(",", ".");
+
+      //Converte para float
+      value = parseFloat(value);
+
+      //Verificar se é um número válido.
+      if (isNaN(value)) {
+        return alert(
+          "Não foi possível calcular o total. Ovalor não parece ser um número."
+        );
+      }
+
+      //Incrementar o valor total.
+      total += Number(value);
+    }
+
+    //Cria span para adicionar R$ formatado.
+    const symbolBRL = document.createElement("small");
+    symbolBRL.textContent = "R$";
+
+    // Formata o valor e remove o R$ QUE SERÁ EXIBIDO PELO SMALL COM UM ESITLO
+    total = formatCurrencyBRL(total).toUpperCase().replace("R$", "");
+
+    // Limpa o conteúdo do elemento
+    expensesTotal.innerHTML = "";
+
+    // Adiciona o símbolo da moeda e o valor total formatado
+    expensesTotal.append(symbolBRL, total);
   } catch (error) {
     alert("Não foi possível atualizar os totais.");
   }
